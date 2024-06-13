6706d82efaedabd716cc590fc5b2c2c86654637f
EkaterinaKugot <kali.kugot@yandex.ru>
2024-06-13 14:55:21 +0800

    Added tests for visualization

---
+++ tests/test_visualization.py
@@ -0,0 +1,45 @@
+import pytest
+from spitec import *
+
+def test_create_site_map():
+    site_map = go.Scattergeo(
+        mode="markers+text",
+        marker=dict(size=8, line=dict(color="gray", width=1)),
+        hoverlabel=dict(bgcolor="white"),
+        textposition="top center",
+        hoverinfo="lat+lon",
+    )
+    fig = go.Figure(site_map)
+    fig.update_layout(
+        title=language["graph-site-map"]["title"],
+        title_font=dict(size=24, color="black"),
+        margin=dict(l=0, t=60, r=0, b=0),
+        geo=dict(projection_type=ProjectionType.MERCATOR.value),
+    )
+    fig.update_geos(
+        landcolor="white",
+    )
+    assert create_site_map() == fig
+
+def test_create_site_data():
+    fig = go.Figure()
+    fig.update_layout(
+        title=language["data-tab"]["graph-site-data"]["title"],
+        title_font=dict(size=24, color="black"),
+        plot_bgcolor="white",
+        margin=dict(l=0, t=60, r=0, b=0),
+        xaxis=dict(
+            title=language["data-tab"]["graph-site-data"]["xaxis"],
+            gridcolor="#E1E2E2",
+            linecolor="black",
+            showline=True,
+            mirror=True,
+        ),
+        yaxis=dict(
+            gridcolor="#E1E2E2",
+            linecolor="black",
+            showline=True,
+            mirror=True,
+        )
+    )
+    assert create_site_data() == fig
