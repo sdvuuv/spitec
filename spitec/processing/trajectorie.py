b38ed8f14d4a1a170c4e2e10882b687144572ea4
Артем Веснин <artem_vesnin@iszf.irk.ru>
2024-10-08 19:09:44 +0800

    Removed simurg_core dependency from trajectorie.py. Added sub_ionospheric() directly

--- spitec/processing/trajectorie.py
+++ spitec/processing/trajectorie.py
@@ -1,8 +1,29 @@
 from spitec.processing.site_processing import Site
 from spitec.processing.data_processing import Sat
-from simurg_core.geospace.sub_ionospheric import sub_ionospheric
 import numpy as np
 from datetime import timedelta
+from numpy import sin, cos, arcsin, pi
+
+RE_km = 6371
+
+def sub_ionospheric(s_lat, s_lon, hm, az, el, R=RE_km):
+    """
+    Calculates subionospheric point and delatas from site
+    Parameters:
+        s_lat, slon - site latitude and longitude in radians
+        hm - ionposheric maximum height (km)
+        az, el - azimuth and elevation of the site-sattelite line of sight in
+            radians
+        R - Earth radius (km)
+    """
+    #TODO use meters
+    psi = pi / 2 - el - arcsin(cos(el) * R / (R + hm))
+    lat = bi = arcsin(sin(s_lat) * cos(psi) + cos(s_lat) * sin(psi) * cos(az))
+    lon = sli = s_lon + arcsin(sin(psi) * sin(az) / cos(bi))
+
+    lon = lon - 2 * pi if lon > pi else lon
+    lon = lon + 2 * pi if lon < -pi else lon
+    return lat, lon
 
 
 class Trajectorie:
